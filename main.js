8a249bd8525608e762d6b0b0310225141eee4266
Isadorasantos16 <149601024+Isadorasantos16@users.noreply.github.com>
2024-04-11 16:46:23 -0300

    rr

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ const dataAtual = new Date();
 let dataNiver = prompt ("Data do próximo niver:");
 dataNiver = new Date (dataNiver + "T23:59:59");
 let diasQueFaltam = Math.floor ((dataNiver-dataAtual)/86400000);
-document.querySelector ("#dias_restantes").textContent= diasQueFaltam;
+document.querySelector ("#dias_restantes").textContent = diasQueFaltam;
